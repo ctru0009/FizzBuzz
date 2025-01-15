65da064e681213d16cf79366dcbd87f7943dad45
Cong Truong <truongcongchuong123@gmail.com>
2025-01-15 17:57:30 +1100

    Remove unused import from GameCard component

--- frontend/src/components/GameCard.tsx
+++ frontend/src/components/GameCard.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState } from "react";
 import GameModal from "./GameModal";
-import GameRule from "@/types/GameRule";
 import Game from "@/types/Game";
 
 interface GameCardProps {
