cb403731f2c6a7da8dfa2155f6053218ad88c1e9
Cong Truong <truongcongchuong123@gmail.com>
2025-01-15 17:55:56 +1100

    Remove unused code.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,3 @@
-import GameList from "@/components/GameList";
-import NavBar from "@/components/NavBar";
 import PlayerGreeting from "@/components/PlayerGreeting";
 
 export default function Home() {
