db32450a40f93886381502c184c8b266d87e1e29
Cong Truong <truongcongchuong123@gmail.com>
2025-01-15 17:55:47 +1100

    Refactor URL constants to use environment variable for base URL

--- frontend/src/const.ts
+++ frontend/src/const.ts
@@ -1,3 +1,6 @@
-const BACKEND_URL = "http://localhost:8080/api";
-const SIGNALR_URL = "http://localhost:8080/sessionHub";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:8080";
+
+const BACKEND_URL = `${BASE_URL}/api`;
+const SIGNALR_URL = `${BASE_URL}/sessionHub`;
 export { BACKEND_URL , SIGNALR_URL };
