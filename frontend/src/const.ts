68b5d17a7490743890f7d0948d7b883d2ef07096
Cong Truong <truongcongchuong123@gmail.com>
2025-02-16 19:15:26 +1100

    Refactor BASE_URL assignment to use a conditional operator for improved clarity

--- frontend/src/const.ts
+++ frontend/src/const.ts
@@ -1,5 +1,5 @@
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:8080";
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ? process.env.NEXT_PUBLIC_BASE_URL: "http://localhost:8080";
 
 const BACKEND_URL = `${BASE_URL}/api`;
 const SIGNALR_URL = `${BASE_URL}/sessionHub`;
